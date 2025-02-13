44798e6f77e0da87553602621e81751be20b4bb8
code-Alireza-code <alireza.mohammadi.m19@gmail.com>
2025-02-13 11:01:25 +0330

    add zod schema validator and modify SendOTP component

--- frontend/src/features/authentication/SendOTPForm.tsx
+++ frontend/src/features/authentication/SendOTPForm.tsx
@@ -1,39 +1,47 @@
 import { useForm } from "react-hook-form";
 import TextField from "../../ui/TextField";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-type SendOTPFormDataType = {
-  phoneNumber: number | string;
-};
+type SendOTPFormDataType = z.infer<typeof validationSchema>;
+
+const validationSchema = z.object({
+  phoneNumber: z.string().nonempty("*لطفا این قسمت را خالی نگذارید"),
+});
 
 function SendOTPForm() {
   const {
     register,
     formState: { errors },
     handleSubmit,
-  } = useForm<SendOTPFormDataType>();
-
+  } = useForm<SendOTPFormDataType>({ resolver: zodResolver(validationSchema) });
+  console.log(errors);
   const handleSendOTP = (formData: SendOTPFormDataType) => {
     console.log(formData);
   };
 
   return (
     <div className="w-full sm:max-w-sm justify-center">
-      <div className="p-8 border border-gray-200">
+      <div className="p-8 border border-gray-200 rounded-lg">
         <div className="text-center text-2xl font-bold text-primary-900 mb-10">
           فریلنسینگ اپ
         </div>
         <div className="font-bold mb-6 text-lg">ورود | ثبت نام</div>
-        <form onSubmit={handleSubmit(handleSendOTP)} className="space-y-10">
+        <form
+          noValidate
+          onSubmit={handleSubmit(handleSendOTP)}
+          className="space-y-10"
+        >
           <TextField
             label="شماره تلفن"
             type="number"
-            {...register("phoneNumber", { required: true })}
+            {...register("phoneNumber")}
             className={`${errors["phoneNumber"] && "border border-error"}`}
             placeholder="شماره خود را وارد کنید..."
           />
           {errors["phoneNumber"] && (
             <span className="text-xs text-error">
-              *لطفا این قسمت را خالی نگذارید
+              {errors.phoneNumber.message}
             </span>
           )}
           <button type="submit" className="btn btn--primary w-full">
