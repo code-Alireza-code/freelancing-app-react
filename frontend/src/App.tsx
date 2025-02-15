874b7f4b46d19262445a7cf4d2e2d69749351604
code-Alireza-code <alireza.mohammadi.m19@gmail.com>
2025-02-15 12:05:05 +0330

    add tanstack-query and axios and config them

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,20 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Auth from "./pages/Auth";
 import { Toaster } from "react-hot-toast";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
-    <div className="container">
-      <Toaster />
-      <Routes>
-        <Route path="/auth" element={<Auth />} />
-      </Routes>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="container">
+        <Toaster />
+        <Routes>
+          <Route path="/auth" element={<Auth />} />
+        </Routes>
+      </div>
+    </QueryClientProvider>
   );
 }
 
