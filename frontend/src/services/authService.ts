6d47c7c98da01bf1893a3361f254b0cd36a9045f
code-Alireza-code <alireza.mohammadi.m19@gmail.com>
2025-02-15 23:35:32 +0330

    fix checkOtpAPI address mistake

--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -5,5 +5,5 @@ export async function getOtpAPI(data: { phoneNumber: string }) {
 }
 
 export async function checkOtpAPI(data: { phoneNumber: string; otp: string }) {
-  return http.post("/user/get-otp", data).then(({ data }) => data.data);
+  return http.post("/user/check-otp", data).then(({ data }) => data.data);
 }
