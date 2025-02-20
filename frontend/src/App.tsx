dabd741120434b8ae3a35120c4884c7c6a5e6f97
code-Alireza-code <alireza.mohammadi.m19@gmail.com>
2025-02-20 22:20:00 +0330

    add useOwnerProjects hook

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import CompleteProfile from "./pages/CompleteProfile";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
-import Owner from "./pages/OwnerDashboard";
 import AppLayout from "./ui/AppLayout";
 import OwnerDashboard from "./pages/OwnerDashboard";
 import Projects from "./pages/Projects";
---
+++ frontend/src/hooks/useProject.ts
@@ -0,0 +1,5 @@
+import { useQuery } from "@tanstack/react-query";
+import { getAllProjectsAPI } from "../services/projectService";
+
+export const useOwnerProjects = () =>
+  useQuery({ queryKey: ["owner-projects"], queryFn: getAllProjectsAPI });
