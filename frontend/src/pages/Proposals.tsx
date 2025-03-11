338dafaa0b6fe261b10fd59fb6fa4eb1e6998f38
code-Alireza-code <alireza.mohammadi.m19@gmail.com>
2025-03-11 12:11:20 +0330

    add other admin routes components

--- frontend/src/pages/Proposals.tsx
+++ frontend/src/pages/Proposals.tsx
@@ -1,11 +1,10 @@
 import ProposalTable from "../features/proposals/ProposalTable";
 
-
 function Proposals() {
   return (
     <div>
       <h1 className="font-black text-secondary-700 text-xl mb-8">
-        پروپوزال های شما
+        لیست پروپوزال ها
       </h1>
       <ProposalTable />
     </div>
