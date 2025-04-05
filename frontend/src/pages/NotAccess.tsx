c12aabd98698b646dd95ae99787ecb998204226a
code-Alireza-code <alireza.mohammadi.m19@gmail.com>
2025-04-05 21:02:51 +0330

    add dark mode feature to NotFound & NotAccess pages

--- frontend/src/pages/NotAccess.tsx
+++ frontend/src/pages/NotAccess.tsx
@@ -1,11 +1,13 @@
 import { useNavigate } from "react-router-dom";
+import { useDarkMode } from "../hooks/useDarkMode";
 
 function NotAccess() {
+  useDarkMode();
   const navigate = useNavigate();
   const handleBackHome = () => navigate("/", { replace: true });
 
   return (
-    <div className="flex items-center h-screen justify-center bg">
+    <div className="flex items-center h-screen justify-center bg-secondary-100 text-secondary-900">
       <div className="max-w-sm flex flex-col items-center justify-center">
         <h1 className="text-[10rem] text-center font-bold">403</h1>
         <h2 className="text-xl font-bold text-secondary-500 mb-4">
--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -1,13 +1,15 @@
 import { useNavigate } from "react-router-dom";
 import useMoveBack from "../hooks/useMoveBack";
+import { useDarkMode } from "../hooks/useDarkMode";
 
 function NotFound() {
+  useDarkMode();
   const navigate = useNavigate();
   const handleBackHome = () => navigate("/", { replace: true });
   const moveBack = useMoveBack();
 
   return (
-    <div className="flex items-center h-screen justify-center bg">
+    <div className="flex items-center h-screen justify-center bg-secondary-100 text-secondary-900">
       <div className="max-w-sm flex flex-col items-center justify-center">
         <h1 className="text-[10rem] text-center font-bold">404</h1>
         <h2 className="text-xl font-bold text-secondary-500 mb-4">
